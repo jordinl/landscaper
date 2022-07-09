19863b0c4e67669c1450b82f95edc3d55c101cfe
Jordi Noguera <jordinoguera83@gmail.com>
2022-07-09 07:56:55 +0200

    Add build command for cncf-landscape

--- packages/changemyname-app/bin/changemyname-bin.js
+++ packages/changemyname-app/bin/changemyname-bin.js
@@ -1,18 +1,13 @@
 #!/usr/bin/env node
 
+const { resolve } = require("path");
+const { execSync } = require("child_process");
+const { symlinkSync, mkdirSync, emptyDirSync, copySync } = require("fs-extra");
+
 const args = process.argv.slice(2);
 const methodName = args[0];
 
-const start = () => {
-  const { resolve } = require("path");
-  const { execSync } = require("child_process");
-  const {
-    symlinkSync,
-    mkdirSync,
-    emptyDirSync,
-    copySync,
-  } = require("fs-extra");
-
+const setUpTmpFolder = () => {
   const templatesDir = resolve(__dirname, "..", "templates");
 
   const targetPath = resolve(".changemyname-tmp");
@@ -23,15 +18,28 @@ const start = () => {
   emptyDirSync(targetPath);
   copySync(templatesDir, targetPath, {});
   symlinkSync(
-    resolve(targetPath, "..", "assets"),
-    resolve(targetPath, "public", "assets")
+      resolve(targetPath, "..", "assets"),
+      resolve(targetPath, "public", "assets")
   );
+}
+
+const start = () => {
+  setUpTmpFolder()
 
   execSync("react-scripts start", { stdio: "inherit" });
 };
 
+const build = () => {
+  const buildPath = resolve('build');
+
+  setUpTmpFolder()
+
+  execSync(`BUILD_PATH="${buildPath}" react-scripts build`, { stdio: "inherit" });
+};
+
 const scripts = {
-  "start": start,
+  start: start,
+  build: build
 };
 
 const method = scripts[methodName];

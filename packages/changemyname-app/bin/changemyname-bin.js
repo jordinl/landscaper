ffeac500551284fa1933c578697121e71391df84
Jordi Noguera <jordinoguera83@gmail.com>
2022-07-16 12:02:47 +0200

    Add prime react and zoom slider

--- packages/changemyname-app/bin/changemyname-bin.js
+++ packages/changemyname-app/bin/changemyname-bin.js
@@ -24,7 +24,7 @@ const start = () => {
       entryPoints: [resolve(packageDir, "dist", "index.js")],
       bundle: true,
       incremental: true,
-      loader: { ".js": "jsx", ".html": "copy" },
+      loader: { ".js": "jsx", ".html": "copy", ".svg": "copy", ".woff": "copy", ".ttf": "copy", ".eot": "copy" },
     }
   )
     .then((result) => {
--- packages/changemyname-app/src/App.js
+++ packages/changemyname-app/src/App.js
@@ -1,9 +1,16 @@
 import React, { useEffect, useState } from "react";
-import "./App.css";
 import { Landscape } from "changemyname-base";
+import { Sidebar } from 'primereact/sidebar';
+import { Slider } from 'primereact/slider';
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+import "./App.css";
 
 function App() {
   const [categories, setCategories] = useState();
+  const [expanded, setExpanded] = React.useState(false);
+  const [zoom, setZoom] = React.useState(100);
 
   useEffect(() => {
     fetch("/assets/landscape.json")
@@ -14,9 +21,17 @@ function App() {
   return (
     categories && (
       <div className="App">
-        <div className="sidebar"></div>
         <div className="landscape">
-          <Landscape categories={categories} />
+          <Landscape categories={categories} zoom={zoom / 100} />
+        </div>
+        <div className="sidebar">
+            <Slider
+                min={100}
+                max={400}
+                step={10}
+                value={zoom}
+                onChange={e => setZoom(e.value)}
+            />
         </div>
       </div>
     )

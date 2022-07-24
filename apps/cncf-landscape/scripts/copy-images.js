ecee5329713381ecbc3532c43ff008b8399836df
Jordi Noguera <jordinoguera83@gmail.com>
2022-07-24 10:39:58 +0200

    Strengthen cncf-landscape setup scripts

--- apps/cncf-landscape/scripts/copy-images.js
+++ apps/cncf-landscape/scripts/copy-images.js
@@ -1,21 +1,28 @@
 import { resolve } from "path";
-import { symlinkSync, unlinkSync, mkdirSync, copyFileSync, existsSync } from "fs";
+import {
+  symlinkSync,
+  unlinkSync,
+  mkdirSync,
+  copyFileSync,
+  existsSync,
+  lstatSync,
+} from "fs";
 
 const srcDir = resolve("original");
 const destDir = resolve("assets");
-const logosDir = resolve(destDir, "logos")
+const logosDir = resolve(destDir, "logos");
 
 if (!existsSync(destDir)) {
   mkdirSync(destDir, { recursive: true });
 }
 
-if (existsSync(logosDir)) {
+if (lstatSync(logosDir, { throwIfNoEntry: false })) {
   unlinkSync(logosDir);
 }
 
 symlinkSync(resolve(srcDir, "cached_logos"), logosDir);
 
 copyFileSync(
-  resolve(srcDir, "images", "cncf-logo.svg"),
-  resolve(destDir, "cncf-logo.svg")
+    resolve(srcDir, "images", "cncf-logo.svg"),
+    resolve(destDir, "cncf-logo.svg")
 );
--- apps/cncf-landscape/scripts/pull-repo.js
+++ apps/cncf-landscape/scripts/pull-repo.js
@@ -5,7 +5,7 @@ import { execSync } from "child_process";
 const repoPath = resolve("original");
 
 const command = existsSync(repoPath)
-  ? `cd original && git pull`
-  : `git clone --depth=1 --single-branch --branch=master https://github.com/cncf/landscape ${repoPath}`;
+    ? `cd original && git reset --hard && git pull`
+    : `git clone --depth=1 --single-branch --branch=master https://github.com/cncf/landscape ${repoPath}`;
 
 execSync(command, { stdio: "inherit" });
